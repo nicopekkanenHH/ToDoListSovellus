d6fbaab5d5c645b6772ac58e819fe17397657d8e
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-10-03 14:13:05 +0300

    Tab

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -7,9 +7,17 @@ import TodoList from "./ToDoList";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
 
   return (
     <Container maxWidth="xl">
@@ -18,10 +26,31 @@ function App() {
         <Toolbar>
           <Typography variant="h6">
             My Todos
-          </Typography>  
+          </Typography>
         </Toolbar>
       </AppBar>
-    <TodoList />
+
+<Tabs value={selectedTab} onChange={handleTabChange} centered>
+        <Tab label="Home" />
+        <Tab label="Todolist" />
+      </Tabs>
+
+      {selectedTab === 0 && (
+        <Box mt={3}>
+          <Typography variant="h4" align="center">
+            Welcome to the Todo App!
+          </Typography>
+          <Typography variant="body1" mt={2} align="center">
+            This is the home page where you can manage your tasks efficiently.
+          </Typography>
+        </Box>
+      )}
+      
+      {selectedTab === 1 && (
+        <Box mt={3}>
+          <TodoList />
+        </Box>
+      )}
     </Container>
   );
 }
